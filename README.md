e875cc9f0bc08d49c1507794e9989874d933513e
guseva-sofya <guseva.sofya.p@gmail.com>
2023-06-24 15:59:54 +0200

    find neighbors of current vertex

---
+++ README.md
@@ -0,0 +1,15 @@
+# Snakes and Ladders
+## Introduction 
+Implement a game - Snakes and Ladders - where we need to find 
+the minimum number of turns to end the game getting from the square 1 to square 100.
+On each turn players will roll a six-sided die and move forward 
+a number of spaces equal to the result. If they land on a square that 
+represents a snake or ladder, they will be transported ahead or behind, 
+respectively, to a new square.
+
+## How to install and run
+
+
+## Materials used for this task:
+1. [Breadth First Search (BFS)](https://youtu.be/xlVX7dXLS64)
+2. [Snakes and Ladders detailed description](https://www.geeksforgeeks.org/snake-ladder-problem-2/)
--- snakes_and_ladders/game.py
+++ snakes_and_ladders/game.py
@@ -8,6 +8,8 @@ Vertex = int
 
 @dataclass
 class QueueItem:
+    """ """
+
     vertex: Vertex
     distance_to_start: int
 
@@ -18,6 +20,15 @@ def find_minimum_number_of_steps(
     snakes: Dict[Vertex, Vertex],
     ladders: Dict[Vertex, Vertex],
 ) -> int:
+    """Finds a minimum number of steps in order to get to the end vertex. Uses
+    breadth first search algorithm.
+
+    Args:
+
+    Returns:
+
+    """
+
     queue: Queue[QueueItem] = Queue()
     queue.put(QueueItem(start, 0))
     visited_vertices: Set[Vertex] = set()
@@ -31,7 +42,7 @@ def find_minimum_number_of_steps(
             visited_vertices.add(current_item.vertex)
 
             neighbors: List[Vertex] = find_neighbors(
-                current_item.vertex, snakes, ladders
+                current_item.vertex, end, snakes, ladders
             )
             for neighbor in neighbors:
                 if neighbor not in visited_vertices:
@@ -43,6 +54,25 @@ def find_minimum_number_of_steps(
 
 
 def find_neighbors(
-    current_vertex: Vertex, snakes: Dict[Vertex, Vertex], ladders: Dict[Vertex, Vertex]
+    current_vertex: Vertex,
+    end_vertex: Vertex,
+    snakes: Dict[Vertex, Vertex],
+    ladders: Dict[Vertex, Vertex],
 ) -> List[Vertex]:
-    return []
+    neighbors: List[Vertex] = []
+    for index in range(1, 7):
+        potential_neighbor = current_vertex + index
+
+        if potential_neighbor > end_vertex:
+            continue
+
+        if potential_neighbor in snakes:
+            snake_tail = snakes[potential_neighbor]
+            neighbors.append(snake_tail)
+        elif potential_neighbor in ladders:
+            ladder_top = ladders[potential_neighbor]
+            neighbors.append(ladder_top)
+        else:
+            neighbors.append(potential_neighbor)
+
+    return neighbors
