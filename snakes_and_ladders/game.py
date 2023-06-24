47c7e995e6b15cf39da1c46dd09ce83aa9606e87
guseva-sofya <guseva.sofya.p@gmail.com>
2023-06-24 13:06:12 +0200

    write breadth first search structure

--- snakes_and_ladders/game.py
+++ snakes_and_ladders/game.py
@@ -1,7 +1,48 @@
-from typing import Dict
+from dataclasses import dataclass
+from queue import Queue
+from typing import List, Dict, Set
+
+
+Vertex = int
+
+
+@dataclass
+class QueueItem:
+    vertex: Vertex
+    distance_to_start: int
 
 
 def find_minimum_number_of_steps(
-    start: int, end: int, snakes: Dict[int, int], ladders: Dict[int, int]
+    start: Vertex,
+    end: Vertex,
+    snakes: Dict[Vertex, Vertex],
+    ladders: Dict[Vertex, Vertex],
 ) -> int:
-    return 0
+    queue: Queue[QueueItem] = Queue()
+    queue.put(QueueItem(start, 0))
+    visited_vertices: Set[Vertex] = set()
+
+    while not queue.empty():
+        current_item = queue.get()
+        if current_item.vertex not in visited_vertices:
+            if current_item.vertex == end:
+                return current_item.distance_to_start
+
+            visited_vertices.add(current_item.vertex)
+
+            neighbors: List[Vertex] = find_neighbors(
+                current_item.vertex, snakes, ladders
+            )
+            for neighbor in neighbors:
+                if neighbor not in visited_vertices:
+                    queue.put(QueueItem(neighbor, current_item.distance_to_start + 1))
+
+    raise Exception(
+        "Could not find shortest path from start to end: this can happen if there is no path between start and end."
+    )
+
+
+def find_neighbors(
+    current_vertex: Vertex, snakes: Dict[Vertex, Vertex], ladders: Dict[Vertex, Vertex]
+) -> List[Vertex]:
+    return []
--- tests/test_snakes_and_ladders.py
+++ tests/test_snakes_and_ladders.py
@@ -1,12 +1,24 @@
+from typing import Dict
 from snakes_and_ladders import game
 
 
 def test_game_without_snakes_and_ladders() -> None:
     start = 0
     end = 99
-    snakes = {}
-    ladders = {}
+    snakes: Dict[game.Vertex, game.Vertex] = {}
+    ladders: Dict[game.Vertex, game.Vertex] = {}
 
     number_of_steps = game.find_minimum_number_of_steps(start, end, snakes, ladders)
 
     assert 17 == number_of_steps
+
+
+def test_game_with_snakes_and_ladders() -> None:
+    start = 0
+    end = 99
+    snakes: Dict[game.Vertex, game.Vertex] = {}
+    ladders = {2: 97}
+
+    number_of_steps = game.find_minimum_number_of_steps(start, end, snakes, ladders)
+
+    assert 2 == number_of_steps
